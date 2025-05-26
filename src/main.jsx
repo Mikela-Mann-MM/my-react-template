2584c1bc13b30a682a5580a0cfc0c01ae0c02304
mikelamann1@gmail.com <mikelamann1@gmail.com>
2025-05-26 21:58:37 +0200

    tanstack tilføjet

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,29 @@ import { createRoot } from 'react-dom/client'
 import './index.scss'
 import { RouterProvider } from 'react-router'
 import router from './router'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5 * 60 * 1000, // 5 minutes
+      cacheTime: 30 * 60 * 1000, // 30 minutes
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    },
+  },
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <QueryClientProvider client={queryClient} />
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />  
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,26 +1,35 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
+import { useQuery } from "@tanstack/react-query";
 
-export default function Dogs() {
-    const [usrs, setUsrs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
+export default function List() {
 
-    useEffect(() => {
+
+   /*  const [usrs, setUsrs] = useState(null);
+    const [isLoading, setIsLoading] = useState(true); */
+
+    const { data, isLoading } = useQuery({
+        queryKey: ["users"],
+        queryFn: () => fetch("http://jsonplaceholder.typicode.com/users").then(response => response.json())
+    }); 
+
+
+    
+    /* useEffect(() => {
         fetch("http://jsonplaceholder.typicode.com/users")
             .then(response => response.json())
             .then(result => setUsrs(result))
             .finally(() => setIsLoading(false));
-    }, []);
+    }, []); */
 
-    return isLoading ? (
-        <p>Loading...</p>
-    ) : (
-        <ul>
-            {usrs.map(user => (
+    return isLoading ? ( <p>Loading...</p>) : (
+        
+         <ul>
+            {data.map(user => (
                 <li key={user.id}>
                     <Link to={`/list/${user.id}`}>{user.name}</Link>
             </li>
             ))}
-        </ul>
+        </ul> 
     );
 }
